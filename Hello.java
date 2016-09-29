0a6b9b39d1c95e287f4fcb6ef814d819ac71daf7
NoahJRedell <noahredell@gmail.com>
2016-09-29 10:25:47 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,12 @@
+ //Hello.java
+
+ class Hello {
+     public static void main (String a[]){
+	for(;;){
+		 System.out.print("Hello world!");
+		 System.out.print("Hello world!");
+	 	System.out.print("Hello world!");
+		}
+	}
+ }
+
